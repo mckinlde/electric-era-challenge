db16cf910f39c8f7f64a81df17d943c79e9c281f
dougiemac (NixOS) <douglas.e.mckinley@gmail.com>
2025-04-25 12:03:55 -0700

    Add flake-based dev environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Electric Era Challenge C++ Dev Environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        gcc
+        cmake
+        make
+        gdb
+        valgrind
+      ];
+
+      shellHook = ''
+        echo "⚡ Welcome to the Electric Era dev shell"
+      '';
+    };
+  };
+}
